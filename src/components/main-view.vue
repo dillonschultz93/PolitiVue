0fe67314486b9b7e33ee61501ae7c7d30ab18686
Trevor <trevcj@gmail.com>
2018-02-27 12:24:10 -0700

    routing

---
+++ src/components/main-view.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="mainVeiw">
+    <div class="yourReps">
+
+    </div>
+
+    <div class="repMap">
+        <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+            &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+            &markers=color:red%7Clabel:C%7C40.718217,-73.998284
+            &key=AIzaSyCTM6kafG8RQwi0qStGmMMqwK30VV52Enc" alt="google static map">
+    </div>
+  </div>
+</template>
+
+<script>
+
+  
+  
+  export default {
+    name: 'mainVeiw',  
+    data() {
+        return {
+            
+        }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  @import "../scss/components/normalize";
+  @import "../scss/components/_typography";
+  @import "../scss/components/_colors";
+  
+  body {
+    margin: 0;
+  }
+  #mainVeiw {
+      display: grid;
+      
+  }
+  
+  
+  
+</style>
