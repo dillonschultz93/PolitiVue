e992c5ed2faa160c2f918bd591530f104ef611b6
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-01 19:37:59 -0700

    Created google maps component

---
+++ src/components/helpers/gooMap.vue
@@ -0,0 +1,53 @@
+<template>
+  <gmap-map
+    :center="center"
+    :zoom="7"
+    style="width: 700px; height: 500px;"
+  >
+    <gmap-marker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    ></gmap-marker>
+    
+  </gmap-map>
+</template>
+ 
+<script>
+  /////////////////////////////////////////
+  // New in 0.4.0
+  import * as VueGoogleMaps from 'vue2-google-maps';
+  import Vue from 'vue';
+ 
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyCTM6kafG8RQwi0qStGmMMqwK30VV52Enc',
+      v: 'OPTIONAL VERSION NUMBER',
+      // libraries: 'places', //// If you need to use place input
+    }
+  });
+ 
+  export default {
+    
+    data () {
+      return {
+        center: {lat: 70.0, lng: 50.0},
+        markers: [{
+          position: {lat: 10.0, lng: 10.0}
+        }, {
+          position: {lat: 11.0, lng: 11.0}
+        }]
+      }
+    }
+  }
+</script> 
+
+
+Add CommentCollapse 
+
+Message Input
+
+Message Justin P. Romanos, Apolonio Garcia, Caleb Harshman, Trevor James
