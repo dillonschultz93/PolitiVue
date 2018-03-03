eabf406d9a9d342b569fda4824370df9e9aeaf14
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-03 16:15:21 -0700

    Added new component that displays the info of each representative

---
+++ src/components/full-representative.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script>
+
+import bus from '../eventBus.js'
+
+export default {
+  name:'fullRep'
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+      getParty: rep => rep.party === "Republican" ? "republican" : rep.party === "Democrat" ? "democrat" : "independent"
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../scss/components/normalize";
+  @import "../scss/components/_typography";
+  @import "../scss/components/_colors";
+  
+  body {
+    margin: 0;
+  }
+  
+</style>
