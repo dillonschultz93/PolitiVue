9f9519de6fbe4743549ec059c25158e1adf70294
Justin <hey@jaypray.com>
2018-03-06 20:53:35 -0700

    added modal -needs styling

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,16 +7,45 @@
       </div>
       
       <div class="nav-items">
-        <a href="#" class="sign-in">Sign In</a>
-        <a href="#" class="create-account">Create an Account</a>
+        <a href="#sign-up-modal" class="sign-in">Sign In</a>
+        <a href="#login-modal" class="create-account">Create an Account</a>
       </div>
     </div>
+
+  <div id="sign-up-modal" class="modalDialog">
+    
+    	<div>
+        <a href="#close" title="Close" class="close">X</a>
+
+
+        <Login />
+      </div>
+  </div>  
+
+
+  <div id="login-modal" class="modalDialog">
+    
+    	<div>
+        <a href="#close" title="Close" class="close">X</a>
+
+        
+        <SignUp />
+      </div>
+  </div>  
+
+
   </div>
 </template>
 
 <script>
+  import SignUp from './sign-up.vue'
+  import Login from './login.vue'
   export default {
-    name: 'navbar'
+    name: 'navbar',
+    components: {
+      SignUp,
+      Login
+    }
   }
 </script>
 
@@ -102,6 +131,60 @@
   .create-account:hover::after {
     width: 100%;
   }
+
+  .modalDialog {
+	position: fixed;
+	font-family: Arial, Helvetica, sans-serif;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background: rgba(0,0,0,0.8);
+	z-index: 99999;
+	opacity:0;
+	-webkit-transition: opacity 400ms ease-in;
+	-moz-transition: opacity 400ms ease-in;
+	transition: opacity 400ms ease-in;
+	pointer-events: none;
+}
+
+.modalDialog:target {
+	opacity:1;
+	pointer-events: auto;
+}
+
+.modalDialog > div {
+	width: 700px;
+	position: relative;
+	margin: 10% auto;
+	padding: 5px 20px 13px 20px;
+	border-radius: 10px;
+	background: #fff;
+	background: -moz-linear-gradient(#fff, #999);
+	background: -webkit-linear-gradient(#fff, #999);
+	background: -o-linear-gradient(#fff, #999);
+}
+
+.close {
+	background: #606061;
+	color: #FFFFFF;
+	line-height: 25px;
+	position: absolute;
+	right: -12px;
+	text-align: center;
+	top: -10px;
+	width: 24px;
+	text-decoration: none;
+	font-weight: bold;
+	-webkit-border-radius: 12px;
+	-moz-border-radius: 12px;
+	border-radius: 12px;
+	-moz-box-shadow: 1px 1px 3px #000;
+	-webkit-box-shadow: 1px 1px 3px #000;
+	box-shadow: 1px 1px 3px #000;
+}
+
+.close:hover { background: #00d9ff; }
   
   @media screen and (max-width: 400px) {
     
