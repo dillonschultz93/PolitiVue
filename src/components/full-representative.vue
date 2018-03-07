87660be4ee7f4350f825ec9c22a54f8ec28f3a40
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-06 18:45:43 -0700

    Displaying representative data

--- src/components/full-representative.vue
+++ src/components/full-representative.vue
@@ -1,7 +1,26 @@
 <template>
   
   <div id="fullRep">
-    <p>{{ this.seletedOfficial.name }}</p>
+    <div class="official">
+      <div class="official-identity">
+        <div class="image-container">
+          <img v-if="this.seletedOfficial.photo" :src="this.seletedOfficial.photo" class="portrait">
+        </div>
+        <div class="official-title">
+          <p v-if="this.seletedOfficial.name && this.seletedOfficial.position" class="official">
+            <span class="official-name">{{ this.seletedOfficial.name }}</span>
+            <br> 
+            <span class="official-positiion">{{ this.seletedOfficial.position }}</span>
+          </p>
+          <p v-if="this.seletedOfficial.party" class="party"> Party: {{ this.seletedOfficial.party }}</p>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="address">
+          <p v-if="this.seletedOfficial.address">{{ this.seletedOfficial.address }}</p>
+        </div>
+      </div>
+    </div>
   </div>
   
 </template>
@@ -33,6 +52,13 @@ export default {
   
   body {
     margin: 0;
+  }
+  
+  .portrait {
+    border-radius: 100%;
+    height: 175px;
+    object-fit: cover;
+    width: 175px;
   }
   
 </style>
