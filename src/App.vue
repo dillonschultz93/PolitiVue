b1ae48f6f05cd6e7e5c003e3ef962f9b687a1338
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-08 11:03:33 -0700

    Added supporting methods for the modal

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,28 @@
 import Nav from "./components/navbar.vue"
 import Jumbo from "./components/jumbotron.vue"
 import Main from "./components/main-view.vue"
-import SignUp from "./components/sign-up.vue"
-import Login from "./components/login.vue"
 // import Footer from "./components/footer.vue"
 
 export default {
   name: 'app',
   data() {
     return {
-      
+      isModalVisible: false,
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true
+    },
+    closeModal() {
+      this.isModalVisible = false
     }
   },
   components: {
     Nav,
     Jumbo,
     Main,
-    SignUp,
-    Login
+    
     // Footer
   }
 }
