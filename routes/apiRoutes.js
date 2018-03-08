d4884aeaa4b1210a1e8e7a4fc59e0a8ffe56cf6e
CalebHarsh <caleb.harsh139@gmail.com>
2018-03-08 13:03:49 -0700

    Cleaned up api Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,52 +4,41 @@ var router = express.Router()
 var api = require("../apiCalls/api.js")
 var bcrypt = require("bcrypt-nodejs")
 
-router.get("/api", (req, res) => {
-  api.federal.getHome().then(results => {
-    console.log(results.data)
-    res.json(results.data)
-  }).catch(err => {
-    console.error(err)
-  }) 
-})
-
 router.get("/api/:zip", (req, res) => {
   api.federal.getRepsByZip(req.params.zip).then(results => {
-    console.log(results.data)
     res.json(results.data)
   }).catch(err => console.error(err))
 })
 
-router.get("/api/elections", (req, res) => {
-  api.federal.getElections().then(results => {
-    res.json(results.data)
-  })
+router.get("api/news", (req, res) => {
+  api.news.getNews("Arizona").then(news => {
+    res.json(news.data)
+  }).catch(err => console.error(err))
 })
 
 router.post("/api/signup", (req, res) => {
-  console.log(req.body)
   db.User.findOne({
     where: {
       email: req.body.email
     }
   }).then(user => {
-    console.log(user)
     if (user) return res.send("User already taken")
     else {
       var userPassword = bcrypt.hashSync(req.body.psw)
-      console.log(userPassword)
       var full_name = req.body.first_name + " " + req.body.last_name
       db.User.create({
         email: req.body.email,
         full_name: full_name,
         zipcode: req.body.zip_code,
-        party: req.body.party,
         password: userPassword
       }).then(result => {
         console.log(result)
         res.send("User Registered")
       })
     }
+  }).catch(err => {
+    console.error(err)
+    res.sendStatus(504)
   })
 })
 
@@ -68,6 +57,9 @@ router.post('/api/signin', (req, res) => {
     else {
       res.send("Password Incorrect")
     }
+  }).catch(err => {
+    console.error(err)
+    res.sendStatus(504)
   })
 })
 
